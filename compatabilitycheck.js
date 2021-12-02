0262fb64d420c763582ebeff96f2493773e5fcee
WestStratfordBuildersInc <85010047+WestStratfordBuildersInc@users.noreply.github.com>
2021-12-02 17:44:13 +0800

    Old files

---
+++ compatabilitycheck.js
@@ -0,0 +1,1 @@
+document.write("Javascript test result: PASS")
---
+++ security.js
@@ -0,0 +1,5 @@
+function requestHandler(req, res) {
+    res.setHeader('X-Frame-Options','NONE');
+}
+
+res.setHeader('X-Frame-Options','NONE');
